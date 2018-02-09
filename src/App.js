924e18c552094876109874ac7765d0d9fdcbacc7
Matthew Mogavero <matt.moga@gmail.com>
2018-02-08 20:27:03 -0500

    set up basic card component

--- src/App.js
+++ src/App.js
@@ -7,12 +7,12 @@ class App extends Component {
     this.state = {
       cards: [
         {
-          src: "https://via.placeholder.com/250x250",
+          src: "https://via.placeholder.com/150x150",
           caption: "some things and stuff",
           isLiked: true
         }, 
         {
-          src: "https://via.placeholder.com/250x250",
+          src: "https://via.placeholder.com/150x150",
           caption: "some other things and stuff",
           isLiked: false
         }
@@ -21,11 +21,17 @@ class App extends Component {
   }
   render() {
     const { cards } = this.state;
+    const CardGrid = cards.map((card, index) => <Card key={index} 
+                                                  src={card.src} 
+                                                  caption={card.caption} 
+                                                  isLiked={card.isLiked} />)
     return (
       <div className="container">
         <h1>CardGrid</h1>
         {/* iteration to make a bunch of cards */}
-        { cards.map(card => <Card />) }
+        <div className="row">
+        { CardGrid }
+        </div>
       </div>
     );
   }
--- src/Card.js
+++ src/Card.js
@@ -4,7 +4,11 @@ import React, { Component } from 'react';
 class Card extends Component {
     render() {
         return (
-            <p>Card</p>
+            <div className="col-md-3">
+                <img src={this.props.src} alt={this.props.caption} />
+                <p>{this.props.caption}</p>
+                <img src="https://via.placeholder.com/20x20" alt="heart" />
+            </div>
         );
     }
 }
